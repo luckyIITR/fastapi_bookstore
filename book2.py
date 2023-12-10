bec8cb4ac284bc82928a587024b60984cae5ce6b
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 18:58:01 +0530

    filter book by rating

--- book2.py
+++ book2.py
@@ -65,6 +65,15 @@ async def read_book(book_id: int):
     raise HTTPException(status_code=404, detail='Item not found')
 
 
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int ):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
