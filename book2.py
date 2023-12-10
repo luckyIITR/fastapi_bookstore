f08b329f976100df396617de6f637bcde7cfafb6
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 18:39:51 +0530

    basic data type validation

--- book2.py
+++ book2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -20,6 +21,15 @@ class Book:
         self.published_date = published_date
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+
+
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
     Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2030),
@@ -33,3 +43,10 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
+    
